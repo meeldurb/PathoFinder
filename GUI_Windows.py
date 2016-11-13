1db6598a1566d44ee04a1741eaf3a42c4f2ad3d9
Melanie vd Bosch <meeldurb@gmail.com>
2016-11-13 21:05:36 +0100

    made almost all windows

--- GUI_Windows.py
+++ GUI_Windows.py
@@ -18,7 +18,7 @@ class OligoDatabase(tk.Tk):
 
         self.frames = {}
 
-        for F in (StartPage, OligosPage, ProjectsPage):
+        for F in (StartPage, OligosPage, ProjectsPage, ExperimentsPage):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="NSEW")
@@ -41,10 +41,26 @@ class StartPage(tk.Frame):
                          command=lambda:controller.show_frame(OligosPage))
         button1.grid()
 
-        button1 = tk.Button(self, text="Projects",
+        button2 = tk.Button(self, text="Projects",
                          command=lambda:controller.show_frame(ProjectsPage))
-        button1.grid()
-        
+        button2.grid()
+
+        button3 = tk.Button(self, text="Employees")
+                             # go to Employees page
+        button3.grid()
+
+        button4 = tk.Button(self, text="Batches")
+                            # go to batches page
+        button4.grid()
+
+        button5 = tk.Button(self, text="Suppliers")
+                            # go to Suppliers page
+        button5.grid()
+
+        button6 = tk.Button(self, text="Experiments",
+                        command=lambda:controller.show_frame(ExperimentsPage))
+                            # go to Experiments page
+        button6.grid()
 
 class OligosPage(tk.Frame):
     def __init__(self, parent, controller):
@@ -57,6 +73,23 @@ class OligosPage(tk.Frame):
                          command=lambda:controller.show_frame(StartPage))
         button1.grid()
 
+        button2 = tk.Button(self, text="View All Oligos")
+                            #goes to table view of all oligos
+        button2.grid()
+
+        button3 = tk.Button(self, text="New Oligo")
+                            #goes to new screen with textfields
+        button3.grid()
+
+        button4 = tk.Button(self, text="Lookup Oligo")
+                            #goes to table view of one oligo
+        button4.grid()
+
+        button5 = tk.Button(self, text="Modify Oligo")
+                            #gos to table view with textfields
+        button5.grid()
+
+        
 class ProjectsPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -67,6 +100,33 @@ class ProjectsPage(tk.Frame):
         button1 = tk.Button(self, text="Back to Home",
                          command=lambda:controller.show_frame(StartPage))
         button1.grid()
+
+class ExperimentsPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        label = tk.Label(self, text="Experiments menu")
+        label.grid(pady=10, padx=10)
+
+        button1 = tk.Button(self, text="Back to Home",
+                         command=lambda:controller.show_frame(StartPage))
+        button1.grid()
+
+        button2 = tk.Button(self, text="View All Experiments")
+                             #show a table view of all experiments
+        button2.grid()
+
+        button3 = tk.Button(self, text="New Experiment")
+                             #show some text fields for adding experiments
+        button3.grid()
+
+        button4 = tk.Button(self, text="Edit Experiments")
+                             #show text fields where you can browse for experiment
+        button4.grid()
+
+        button5 = tk.Button(self, text="Lookup")
+                             #show text fields where you can lookup
+        button5.grid()
 
 
 if __name__ == "__main__":
