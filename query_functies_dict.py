a7de8daca23b2c78183b987417f50dff4fdd5db4
jornvdent <jornvdent@live.nl>
2016-12-29 16:32:58 +0100

    checked most sql, a few to go

--- query_functies_dict.py
+++ query_functies_dict.py
@@ -249,109 +249,97 @@ def search(table_str, attributes_list, search_input): # works
 # vanaf hier moeten ze gecheckt worden met de nieuwe database (komen namelijk uit het verslag)
 # oligo and batches
 
-def select_batch_oligo():
+def join_batch_oligo(): #works
     """Returns a tuple of the join between oligo and batch"""
     
     sql = "SELECT * FROM `oligo`, `batch` WHERE `oligo`.oligo_ID = `batch`.oligo_ID"
-    return execute_select_queries(query)
+    return execute_select_queries(sql)
 
-def oligo_batch_info(oligo_ID):
+def oligo_batch_info(oligo_ID): #works
     """Returns a tuple of all the batches and related information
         from the given oligo
 
     Keyword Arguments:
     oligo_ID    -- string, the key of the oligo for which you want retrieve the information."""
 
-    sql =   "SELECT batch_number, synthesis_level_ordered, synthesis_level_delivered,\
+    sql =   "SELECT `oligo`.oligo_ID, batch_number, synthesis_level_ordered, synthesis_level_delivered,\
             purification_method, spec_sheet_location, order_number, delivery_date,\
             order_status FROM oligo, batch WHERE oligo.oligo_ID = batch.oligo_ID \
-            AND batch.oligo_ID = %s" % oligo_ID
+            AND batch.oligo_ID = '%s'" % oligo_ID
     return execute_select_queries(sql)
 
 
 # Experiments analysis
 
-def testlist_oligo(oligo_ID):
+def testlist_oligo(oligo_ID): #works
     """Returns a tuple of all tests in which the given oligo was used
 
     Keyword Arguments:
     oligo_ID    -- string, the key of the oligo for which you want retrieve the information."""
-    oligo = tuple([oligo_ID]*3)
-    sql = "SELECT approval.experiment_ID, test_number, lab_report, experiment_date,\
-        oligo_ID_fwd, oligo_ID_rev, oligo_ID_probe,\
-        approved_status FROM experiment, approval\
-        WHERE experiment.experiment_ID = approval.experiment_ID\
-        AND (approval.oligo_ID_fwd = %s \
-        OR approval.oligo_ID_rev = %s \
-        OR approval.oligo_ID_probe = %s)" % oligo
+    oligo = tuple([oligo_ID]*5)
+    sql = "SELECT DISTINCT experiment.experiment_ID, test_number, lab_report, experiment_date, \
+            oligo_ID_fwd, oligo_ID_rev, oligo_ID_probe, oligo_ID_4, oligo_ID_5, approved_status\
+            FROM experiment, approval, `lab_report`\
+            WHERE experiment.experiment_ID = approval.experiment_ID\
+            AND (approval.oligo_ID_fwd = '%s'\
+            OR approval.oligo_ID_rev = '%s'\
+            OR approval.oligo_ID_probe = '%s'\
+            OR approval.oligo_ID_4 = '%s'\
+            OR approval.oligo_ID_5 = '%s')" % oligo
     return execute_select_queries(sql)
-	
-def experimentlist_approval():
-    """Query for showing a list of all experiments (ordered by date)
-        and their results regarding their approval."""
 
-    sql = "SELECT * FROM experiment, approval \
-        WHERE experiment.experiment_ID = approval.experiment_ID \
-        ORDER BY experiment_date"
-    return execute_select_queries(sql)
-    
-def oligolist_test_results():
-
-    sql = "SELECT DISTINCT oligo.oligo_ID, oligo_name,\
-        COUNT(IF(approved_status='approved',1,NULL)) AS Approved,\
-        COUNT(IF(approved_status='neutral',1,NULL)) AS Neutral,\
-        COUNT(IF(approved_status='disapproved',1,NULL)) AS Disapproved,\
-        ROUND((COUNT(IF(approved_status='approved',1,NULL))/\
-        (COUNT(IF(approved_status='approved',1,NULL))+COUNT(IF(approved_status=\
-        disapproved,1,NULL))+COUNT(IF(approved_status='neutral',1,NULL))) *\
-        100),0) AS 'Percentage of approval'\
-        FROM oligo, approval\
-        WHERE (oligo.oligo_ID = approval.oligo_ID_fwd\
-        OR oligo.oligo_ID = approval.oligo_ID_rev\
-        OR oligo.oligo_ID = approval.oligo_ID_probe\
-        GROUP BY oligo_ID"
+def oligolist_test_results(): #works
+    """Returns a tuple of a summary of the test results for all oligos"""
+    sql = "SELECT DISTINCT oligo.oligo_ID, oligo_name, COUNT(IF(approved_status='approved',1,NULL))\
+            AS Approved, COUNT(IF(approved_status='neutral',1,NULL)) AS Neutral,\
+            COUNT(IF(approved_status='disapproved',1,NULL)) AS Disapproved,\
+            ROUND((COUNT(IF(approved_status='approved',1,NULL))/\
+            (COUNT(IF(approved_status='approved',1,NULL))+COUNT(IF(approved_status='disapproved',1,NULL))\
+            + COUNT(IF(approved_status='neutral',1,NULL))) *100),0) AS 'Percentage of approval' \
+            FROM oligo, approval WHERE (oligo.oligo_ID = approval.oligo_ID_fwd \
+            OR oligo.oligo_ID = approval.oligo_ID_rev OR oligo.oligo_ID = approval.oligo_ID_probe \
+            OR oligo.oligo_ID = approval.oligo_ID_4 OR oligo.oligo_ID = approval.oligo_ID_5) \
+            GROUP BY oligo_ID"
     return execute_select_queries(sql)
 
 # Supplier evaluation
 
-def supplier_synthesis_quality():
+def supplier_synthesis_quality(): #works
     """Query for showing the average difference in synthesis level per supplier,
-    indicating itsquality"""
+    indicating its quality"""
 
-    sql = "SELECT DISTINCT supplier_name, ROUND(AVG(synthesis_level_ordered -\
-            synthesis_level_delivered),2) AS Difference\
-            FROM batch, groupwork.`order`, supplier\
-            WHERE batch.order_number = groupwork.`order`.order_number\
-            AND groupwork.`order`.supplier_ID = supplier.supplier_ID\
+    sql = "SELECT supplier_name, ROUND(AVG(synthesis_level_ordered - synthesis_level_delivered),2) AS Difference \
+            FROM batch, `order`, supplier \
+            WHERE batch.order_number = `order`.order_number \
+            AND `order`.supplier_ID = supplier.supplier_ID \
             GROUP BY supplier_name"
     return execute_select_queries(sql)
 
-def supplier_delivery_time():
+def supplier_delivery_time(): # need unified entry of dates in the database, worked with test data!
     """Query for calculating the average delivery time per supplier."""
-
-    sql = "SELECT supplier_name, ROUND(AVG(delivery_date - order_date),1)\
-            FROM batch, groupwork.`order`, supplier\
-            WHERE batch.order_number = groupwork.`order`.order_number AND\
-            groupwork.`order`.supplier_ID = supplier.supplier_ID\
-            GROUP BY supplier_name"
+    sql = "SELECT supplier_name, avg(DATEDIFF(str_to_date(batch.delivery_date, '%d-%m-%Y'),\
+            str_to_date(`order`.order_date, '%d-%m-%Y'))) As difference\
+            FROM  supplier, batch, `order`\
+            where batch.order_number = `order`.order_number AND `order`.supplier_ID = supplier.supplier_ID\
+            Group By supplier.supplier_name;  
     return execute_select_queries(sql)
 
 # Project and the linked oligos
 
-def oligos_from_project(project_name):
+def oligos_from_project(project_name): #for some weird reason it gives an ERROR: EOL while scanning string literal, at the beginning of the sql statement 
     """Query for creating a table with the oligos from the given project
 
     Keyword Arguments:
     project_name  -- string, the name of the project (not ID!) in the project table """
 
-    sql = "SELECT oligo.oligo_ID, oligo_name, oligo_type, sequence, description,\
-            entry_date, creator, update_date, modifier, label5prime, label3prime,\
-            labelM1, labelM1position, notes\
+    sql = "SELECT `oligo`.oligo_ID, oligo_name, oligo_type, sequence, description,\
+            entry_date, creator, update_date, modifier, label5prime,\
+            label3prime, labelM1, labelM1position, notes\
             FROM oligo, project_oligo, project\
             WHERE oligo.oligo_ID = project_oligo.oligo_ID\
             AND project_oligo.project_ID = project.project_ID\
-            AND project.project_name = %s\
-            ORDER BY entry_date DESC;" % project_name
+            AND project.project_name = '%s'\
+            ORDER BY entry_date DESC" % project_name
     return execute_select_queries(sql)
 
 def find_approved_oligos_for_project(project_name):
@@ -381,22 +369,28 @@ def get_date_stamp():
     date = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y')
     return date
 
-
-    
+  
 
 if __name__ == "__main__":
     host = '127.0.0.1'
     user = 'root'
     password = 'root'
-    database = 'groupwork'
+    database = 'pathofinder_db'
     #insert_row('Oligo', { 'oligo_ID' : 'OlI0012', 'oligo_name' : 'test', 'oligo_type': '', 'sequence' : 'J', 'description':'',  'entry_date':'', 'creator':'EMP', 'update_date':'', 'modifier':'EMP', 'label5prime':'', 'label3prime':'', 'labelM1':'', 'labelM1position':'', 'pathogen_name':'', 'target':'', 'notes':''})
     #delete_row('Employee', { 'employee_ID' : 'EMP0066', 'emp_name' : 'test20'})
     #update_row('Employee', { 'employee_ID' : 'EMP0066', 'emp_name' : 'test20'}, { 'employee_ID' : 'EMP0065', 'emp_name' : 'test19'})
     #print search_in_single_attribute('Employee', 'employee_ID', 'EMP0007')
     #print search_entire_table('Employee', ['employee_ID', 'emp_name'], 'EMP0063')
     #print search("Employee", ['employee_ID', 'emp_name'], "EMP 0000")
-    print search('Oligo', ['oligo_ID', 'oligo_name', 'oligo_type', '\
-                 sequence', 'description', 'entry_date', 'creator', '\
-                 update_date', 'modifier', 'label5prime', 'label3prime', '\
-                 labelM1', 'labelM1position', 'notes'], "A test OLI0006")
+    #print search('Oligo', ['oligo_ID', 'oligo_name', 'oligo_type', '\
+    #             sequence', 'description', 'entry_date', 'creator', '\
+    #             update_date', 'modifier', 'label5prime', 'label3prime', '\
+    #             labelM1', 'labelM1position', 'pathogen_name', 'target', 'notes'], "OLI000006")
     #oligo_to_temp_bin('OLI0013')
+    #print oligo_batch_info('OLI000088')
+    #print join_batch_oligo()
+    #print testlist_oligo('OLI000018')
+    #print oligolist_test_results()
+    #print supplier_synthesis_quality()
+    print oligos_from_project('something')
+    
