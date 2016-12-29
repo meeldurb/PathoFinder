e6c4966dcbe14409402565f7dc9f1445ba0a64dd
Melanie vd Bosch <meeldurb@gmail.com>
2016-12-29 17:21:59 +0100

    converting import oligo file to dictionary to make it compatible to import in sql database

---
+++ import_oligo_parser.py
@@ -0,0 +1,42 @@
+"""
+Author: Melanie van den Bosch
+Script for parsing import oligos into dictionary
+Script "query_functies_as_dictionary.py" puts these inside
+SQL database
+"""
+
+import csv
+import query_functies_as_dictionary
+
+def open_file(filename):
+    """ Opens a file and reads it"""
+
+    file_content = open(filename, "r")
+    # oligoreader = csv.reader(file_content, delimiter=';')
+    return file_content
+
+
+def parse_oligofile(filename):
+    """ Returns the cells of the oligo import file to a dictionary
+
+    Keyword arguments:
+        filename -- string, the filename of the import oligo file
+
+    Returns:
+        oligo_dict -- dictionary, a dictionary of the data that will be imported
+        into the database
+    """
+    import_data = open_oligofile(filename)
+    import_dict = {}
+    # read in the column names and make dictionary keys
+    header = import_data.readline().strip()
+    header_colnames = header.split(";")
+    print header_colnames
+    for name in header_colnames:
+        import_dict[name] = ""
+    print import_dict
+    # Every row fills the values of the dict
+    # at the end it is imported to the database and emptied
+        for row in import_dict:
+            
+   
